8ac63e794d5a9c93fa964bfa1846319869d85813
FahrJo <johannes.fahr@posteo.de>
2023-03-23 20:05:16 +0100

    formating and refactoring

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,33 +1,35 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 
-
 suite('Ultimate Extension Test Suite', () => {
-	let extensionContext: vscode.ExtensionContext;
+    let extensionContext: vscode.ExtensionContext;
 
-	suiteSetup(async () => {
+    suiteSetup(async () => {
         // Trigger extension activation and grab the context as some tests depend on it
         const extension = vscode.extensions.getExtension('FahrJo.ultimate-automizer');
-		extensionContext = await extension?.activate();
+        extensionContext = await extension?.activate();
     });
 
-	test('outputChannelsSubscribedTest', () => {
-		let logSubscriberOk = false;
-		let outSubscriberOk = false;
+    test('outputChannelsSubscribedTest', () => {
+        let logSubscriberOk = false;
+        let outSubscriberOk = false;
 
-		extensionContext.subscriptions.forEach( (subscription) => {
-			if (isOutputChannel(subscription)) {
-				logSubscriberOk = (subscription.name.match(/^Ultimate Log$/) ? true : logSubscriberOk);
-				outSubscriberOk = (subscription.name.match(/^Ultimate Results$/) ? true : outSubscriberOk);
-			}
-		});
-		
-		assert.strictEqual(logSubscriberOk, true);
-		assert.strictEqual(outSubscriberOk, true);
-	});
+        extensionContext.subscriptions.forEach((subscription) => {
+            if (isOutputChannel(subscription)) {
+                logSubscriberOk = subscription.name.match(/^Ultimate Log$/)
+                    ? true
+                    : logSubscriberOk;
+                outSubscriberOk = subscription.name.match(/^Ultimate Results$/)
+                    ? true
+                    : outSubscriberOk;
+            }
+        });
+
+        assert.strictEqual(logSubscriberOk, true);
+        assert.strictEqual(outSubscriberOk, true);
+    });
 });
 
-
 function isOutputChannel(obj: any | vscode.OutputChannel): obj is vscode.OutputChannel {
-	return 'name' in obj;
+    return 'name' in obj;
 }
